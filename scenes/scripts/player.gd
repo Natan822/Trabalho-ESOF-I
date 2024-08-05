0782c47ad9699b6b83a7c383e9d4753263659562
Natan822 <natanmiguel821@gmail.com>
2024-08-04 22:23:03 -0300

    feat: adiciona animações de andar do personagem

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_FORCE = -400.0
 
+@onready var walk_animations: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite_gnomo: Sprite2D = $Sprite2D
+
 # Gravidade padrao do objeto 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -14,17 +17,34 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# se o player estiver no chao e for pressionado a tecla espaco aplica a jump_force
-	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
-
+	
 	# movimenta o player de acordo com a tecla de direcao pressionada
-	
-	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		sprite_gnomo.visible = false
+		walk_animations.visible = true
 		velocity.x = direction * SPEED
+		
+		# andando para direita
+		if direction == 1:
+			sprite_gnomo.flip_h = false
+			walk_animations.flip_h = false
+		
+		# andando para esquerda
+		else:
+			sprite_gnomo.flip_h = true
+			walk_animations.flip_h = true
+			
+		walk_animations.play("walk")
+	# esta parado
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		walk_animations.stop()
+		walk_animations.visible = false
+		sprite_gnomo.visible = true
+	
+		
+	
 	move_and_slide()
