0b8e2ffc86190508beb2860d2024e58389d9020b
Natan822 <natanmiguel821@gmail.com>
2024-08-06 13:47:16 -0300

    feat: atualiza animacao de personagem parado

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
-
+# Constante de velocidade do personagem
 const SPEED: float = 300.0
+# Constante de força de pulo do personagem
 const JUMP_FORCE: float = -400.0
 
+# Variável que controla animações do gnomo
 @onready var animations_gnomo: AnimatedSprite2D = $AnimatedSprite2D
+# Variável que controla sprite do gnomo
 @onready var sprite_gnomo: Sprite2D = $Sprite2D
 
 # Gravidade padrao do objeto 
@@ -16,31 +19,31 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# se o player estiver no chao e for pressionado a tecla espaco aplica a jump_force
+	# Se o player estiver no chao e for pressionado a tecla espaco aplica a jump_force
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
 	
-	# movimenta o player de acordo com a tecla de direcao pressionada
+	# Movimenta o player de acordo com a tecla de direcao pressionada
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		sprite_gnomo.visible = false
 		animations_gnomo.visible = true
 		velocity.x = direction * SPEED
 		
-		# andando para direita
+		# Andando para direita
 		if direction == 1:
 			sprite_gnomo.flip_h = false
 			animations_gnomo.flip_h = false
 		
-		# andando para esquerda
+		# Andando para esquerda
 		else:
 			sprite_gnomo.flip_h = true
 			animations_gnomo.flip_h = true
 			
 		animations_gnomo.play("walk")
-	# esta parado
+	# Está parado
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animations_gnomo.play("idle")
+		animations_gnomo.play("idle_novo")
 		
 	move_and_slide()
