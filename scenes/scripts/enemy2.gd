1f1b0d228d7f6ad30fb4750000df9a14a81b0008
Natan822 <natanmiguel821@gmail.com>
2024-09-01 16:32:09 -0300

    feat: adiciona inimigos ao redor do mapa

--- scenes/scripts/enemy2.gd
+++ scenes/scripts/enemy2.gd
@@ -10,17 +10,19 @@ enum State {
 }
 
 # Constante de velocidade do personagem
-const SPEED: float = 50.0
+const SPEED: float = 40.0
 # Constante de força de pulo do personagem
-const JUMP_FORCE: float = -200.0
+const JUMP_FORCE: float = -175.0
 
 # Variável que controla animações do personagem
 @onready var animations: AnimatedSprite2D = $AnimatedSprite2D
-# Variável que detecta colisao de personagem com parede
-@onready var wall_detector: RayCast2D = $RayCast2D
+# Variável que detecta paredes
+@onready var wall_detector: RayCast2D = $"RayCast2D(parede)"
+# Variável que detecta chão
+@onready var ground_detector: RayCast2D = $"RayCast2D(chao)"
 
 # Gravidade padrao do objeto 
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity") / 2
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity") / 3
 # Estado inicial do personagem
 var _state : State = State.IDLE
 
@@ -67,9 +69,16 @@ func _physics_process(delta: float) -> void:
 	
 	if wall_detector.is_colliding():
 		wall_detector.target_position.x *= -1
+		ground_detector.target_position.x *= -1
 		direction *= -1
 		animations.flip_h = !animations.flip_h
-		
+	
+	if  is_on_floor() and !ground_detector.is_colliding():
+		wall_detector.target_position.x *= -1
+		ground_detector.target_position.x *= -1
+		direction *= -1
+		animations.flip_h = !animations.flip_h
+	
 	velocity.x = SPEED * direction
 	
 	
